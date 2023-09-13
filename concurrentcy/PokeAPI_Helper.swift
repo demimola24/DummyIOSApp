b8152604a22fde81a49dd3b7e885e7f87b892683
JV658 <90655082+JV658@users.noreply.github.com>
2023-09-13 16:46:09 -0400

    committing

---
+++ concurrentcy/PokeAPI_Helper.swift
@@ -0,0 +1,16 @@
+//
+//  PokeAPI_Helper.swift
+//  concurrentcy
+//
+//  Created by Cambrian on 2023-09-11.
+//
+
+import Foundation
+
+class PokeAPI_Helper{
+    static private let baseURL_String = "https://pokeapi.co/api/v2/"
+    
+    static public func fetch() -> Data{
+        
+    }
+}
--- concurrentcy/ViewController.swift
+++ concurrentcy/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var callPokeAPI: UIButton!
+    
+    @IBAction func callPokeApi(_ sender: Any) {
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
 
